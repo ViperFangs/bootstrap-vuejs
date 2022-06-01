60fbbf2d7ba06d056f805b0b82c1e5ea05789956
Aarya <amathrej@asu.edu>
2022-05-31 23:33:25 -0700

    Fix vue.config.js

--- landing-page/vue.config.js
+++ landing-page/vue.config.js
@@ -1,5 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  base: '/bootstrap-vuejs/'
-  transpileDependencies: true
-})
+	base: '/bootstrap-vuejs/',
+	transpileDependencies: true
+});
