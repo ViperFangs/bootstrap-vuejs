7ca60e3992c203d27fa2b3f17ec6f9f64b5683f7
Aarya <amathrej@asu.edu>
2022-05-31 23:14:31 -0700

    Add footer text

--- landing-page/src/components/FooterBar.vue
+++ landing-page/src/components/FooterBar.vue
@@ -1,6 +1,9 @@
 <template>
+  <footer class="footer mt-5 py-3 bg-dark">
   <div class="container">
+    <span class="text-white">Created by <a href="https://www.linkedin.com/in/aarya-mathreja">Aarya Mathreja</a></span>
   </div>
+</footer>
 </template>
 
 <script>
