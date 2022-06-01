dd422aeb27d534d7fe1bc4890578bad4b05fee33
Aarya <amathrej@asu.edu>
2022-05-31 22:19:46 -0700

    Add jumbotron.vue

---
+++ landing-page/src/components/Jumbotron.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+        <img src="https://picsum.photos/200/300" alt="picsum photos" />
+      </div>
+      <div class="col-md-5">
+        <h1 class="font-weight-light">KAR Global</h1>
+        <p class="mt-4">
+        This is a Bootstrap + VueJS Landing Page 
+        I have created this demo webpage to demonstrate
+        that I understand the basic concepts of both these frameworks.
+        Feel free to ask me any questions!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Jumbotron',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
