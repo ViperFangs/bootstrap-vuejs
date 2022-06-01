07c4cb7fbf7aa14b6c8e4a1143f1734bf136186e
Aarya <amathrej@asu.edu>
2022-05-31 22:01:21 -0700

    Add Bootstrap navbar

--- landing-page/src/App.vue
+++ landing-page/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavBar msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavBar
   }
 }
 </script>
