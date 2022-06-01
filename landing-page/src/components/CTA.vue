cf9765dc584e02b8df98bf499be089f6561c2c73
Aarya <amathrej@asu.edu>
2022-05-31 22:49:08 -0700

    Add CTA template body

--- landing-page/src/components/CTA.vue
+++ landing-page/src/components/CTA.vue
@@ -1,5 +1,18 @@
 <template>
+  <div class="container">
+    <div class="row">
+    </div>
 
+    <div class="col">
+      <div class="bg-dark text-white card my-5 py-4 card">
+        <div class="card-body">
+          I am currently learning Full Stack Development on <a href="https://theodinproject.com">The Odin Project</a>.
+          I am a quick learner, It took me 4 days to learn Ruby!
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
